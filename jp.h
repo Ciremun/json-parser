cfc3c92bf04ee08db8e95d7f85c5f9fdb9440a91
Ciremun <Ciremun@yandex.ru>
2021-08-18 03:25:52 +0300

    doesnt wrok

---
+++ jp.h
@@ -0,0 +1,471 @@
+#ifndef JP_H_
+#define JP_H_
+
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
+
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#else
+#include <sys/mman.h>
+#include <errno.h>
+#endif
+
+#ifdef NDEBUG
+#define JP_PANIC(fmt, ...) exit(1);
+#define JP_PANIC_MSG(msg) exit(1);
+#define JP_PANIC_RAW() exit(1);
+#else
+#include <stdio.h>
+#define JP_PANIC(fmt, ...)                                      \
+    do                                                          \
+    {                                                           \
+        printf("[ERRO] L%d: " fmt "\n", __LINE__, __VA_ARGS__); \
+        exit(1);                                                \
+    } while (0)
+#endif
+#define JP_PANIC_MSG(msg)                          \
+    do                                             \
+    {                                              \
+        printf("[ERRO] L%d: " msg "\n", __LINE__); \
+        exit(1);                                   \
+    } while (0)
+#define JP_PANIC_RAW()                    \
+    do                                    \
+    {                                     \
+        printf("[ERRO] L%d\n", __LINE__); \
+        exit(1);                          \
+    } while (0)
+
+static size_t system_memory_size = 0;
+
+typedef struct JPair JPair;
+
+typedef enum
+{
+    JSON_OBJECT = 0,
+    JSON_STRING,
+    JSON_BOOL,
+} JType;
+
+typedef enum
+{
+    TOKEN_KIND_NONE = 0,
+    TOKEN_KIND_OPEN_CURLY,
+    TOKEN_KIND_CLOSE_CURLY,
+    TOKEN_KIND_OPEN_QUOTATION,
+    TOKEN_KIND_CLOSE_QUOTATION,
+    TOKEN_KIND_COLON,
+    TOKEN_KIND_COMMA
+} TokenKind;
+
+typedef enum
+{
+    STATE_NONE = 0,
+    STATE_KEY,
+    STATE_VALUE
+} State;
+
+typedef struct
+{
+    JPair *pairs;
+    size_t pairs_count;
+} JObject;
+
+typedef struct
+{
+    JType type;
+    union
+    {
+        JObject object;
+        int number;
+        int boolean;
+        char *string;
+        int null;
+    };
+} JValue;
+
+struct JPair
+{
+    char *key;
+    JValue *value;
+};
+
+typedef struct
+{
+    char *start;
+    char *base;
+#ifdef _WIN32
+    size_t commited;
+    size_t allocated;
+    size_t commit_size;
+#endif // _WIN32
+} Memory;
+
+typedef struct
+{
+    Memory json_memory;
+    size_t pairs_total;
+    size_t pairs_commited;
+} JParser;
+
+#ifndef _WIN32
+int GetPhysicallyInstalledSystemMemory(size_t *output)
+{
+    FILE *meminfo = fopen("/proc/meminfo", "r");
+    if (meminfo == NULL)
+        JP_PANIC_MSG("Error opening file '/proc/meminfo'");
+
+    char line[256];
+    while (fgets(line, sizeof(line), meminfo))
+    {
+        if (sscanf(line, "MemTotal: %zu kB", output) == 1)
+        {
+            fclose(meminfo);
+            return 1;
+        }
+    }
+
+    fclose(meminfo);
+    return 0;
+}
+#endif // _WIN32
+
+void memory_init(Memory *memory)
+{
+    if (system_memory_size == 0)
+    {
+        if (!GetPhysicallyInstalledSystemMemory(&system_memory_size))
+            system_memory_size = INTPTR_MAX == INT32_MAX ? 4294967295ULL : 16ULL * 1024 * 1024 * 1024;
+        else
+            system_memory_size *= 1024;
+    }
+#ifdef _WIN32
+    memory->commited = 0;
+    memory->allocated = 0;
+    memory->commit_size = 1024;
+    memory->base = (char *)(VirtualAlloc(NULL, system_memory_size, MEM_RESERVE, PAGE_READWRITE));
+    if (memory->base == NULL)
+        JP_PANIC("VirtualAlloc failed: %lu", GetLastError());
+#else
+    memory->base = (char *)(mmap(0, system_memory_size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0));
+    if (memory->base == MAP_FAILED)
+        JP_PANIC("mmap failed: %s", strerror(errno));
+#endif // _WIN32
+    memory->start = memory->base;
+}
+
+void *memory_alloc(Memory *memory, size_t size)
+{
+#ifdef _WIN32
+    memory->allocated += size;
+    if (memory->allocated * 2 > memory->commited)
+    {
+        memory->commit_size += size * 2;
+        if (VirtualAlloc(memory->start, memory->commit_size, MEM_COMMIT, PAGE_READWRITE) == NULL)
+            JP_PANIC("VirtualAlloc failed: %lu", GetLastError());
+        memory->commited += memory->commit_size;
+    }
+#endif // _WIN32
+    memory->start += size;
+    return memory->start - size;
+}
+
+void memory_free(Memory *memory)
+{
+#ifdef _WIN32
+    if (VirtualFree(memory->base, 0, MEM_RELEASE) == 0)
+        JP_PANIC("VirtualFree failed: %lu", GetLastError());
+#else
+    if (munmap(memory->base, system_memory_size) == -1)
+        JP_PANIC("munmap failed: %s", strerror(errno));
+#endif // _WIN32
+}
+
+void jobject_init(JObject *jobject, JPair *pairs)
+{
+    jobject->pairs = pairs;
+    jobject->pairs_count = 0;
+}
+
+void jobject_add_pair(JObject *jobject, char *key, JValue *value)
+{
+    jobject->pairs[jobject->pairs_count].key = key;
+    jobject->pairs[jobject->pairs_count].value = value;
+    jobject->pairs_count++;
+}
+
+JValue jobject_get(JObject *jobject, const char *key)
+{
+    for (size_t i = 0; i < jobject->pairs_count; ++i)
+    {
+        if (strcmp(key, jobject->pairs[i].key) == 0)
+            return *(jobject->pairs[i].value);
+    }
+    JP_PANIC("key \"%s\" was not found", key);
+}
+
+#endif // JP_H_
+
+#ifdef JP_IMPLEMENTATION
+
+void jparser_init(JParser *jparser, const char *input)
+{
+    memory_init(&jparser->json_memory);
+    jparser->pairs_total = 0;
+    jparser->pairs_commited = 0;
+    for (size_t i = 0; input[i] != 0; ++i)
+        if (input[i] == ':' && input[i - 1] == '"' && input[i - 2] != '\\')
+            jparser->pairs_total++;
+    memory_alloc(&jparser->json_memory, sizeof(JPair) * jparser->pairs_total);
+}
+
+JObject parse_object(JParser *jparser, const char *input)
+{
+    TokenKind current_token_kind = TOKEN_KIND_NONE;
+    State state = STATE_NONE;
+
+    char *current_key = NULL;
+    JValue *current_value = NULL;
+
+    size_t pairs_offset = sizeof(JPair) * jparser->pairs_commited;
+    JPair *pairs_start = (JPair *)(jparser->json_memory.base + pairs_offset);
+    JObject json;
+    jobject_init(&json, pairs_start);
+
+    for (size_t pos = 0; input[pos] != '\0'; ++pos)
+    {
+        if (input[pos] == ' ' ||
+            input[pos] == '\t' ||
+            input[pos] == '\n' ||
+            input[pos] == '\r')
+            continue;
+        switch (input[pos])
+        {
+        case 't':
+        {
+            switch (current_token_kind)
+            {
+            case TOKEN_KIND_COLON:
+            {
+                size_t start_pos = pos;
+                do
+                {
+                    pos++;
+                    if (input[pos] == '\0')
+                        JP_PANIC("unexpected end of file at %zu", pos);
+                } while (pos - start_pos != 4);
+                char *bool_value = (char *)memory_alloc(&jparser->json_memory, 5);
+                memcpy(bool_value, input + start_pos, 4);
+                bool_value[4] = '\0';
+                if (strcmp(bool_value, "true") == 0)
+                {
+                    current_value = (JValue *)memory_alloc(&jparser->json_memory, sizeof(JValue));
+                    current_value->type = JSON_BOOL;
+                    current_value->boolean = 1;
+                    current_token_kind = TOKEN_KIND_CLOSE_QUOTATION;
+                    state = STATE_NONE;
+                    pos--;
+                    break;
+                }
+                JP_PANIC_MSG("failed to parse 'true'");
+            }
+            break;
+            default:
+                goto process_char;
+            }
+        }
+        break;
+        case '"':
+        {
+            switch (current_token_kind)
+            {
+            case TOKEN_KIND_COMMA:
+            case TOKEN_KIND_OPEN_CURLY:
+            {
+                current_token_kind = TOKEN_KIND_OPEN_QUOTATION;
+                state = STATE_KEY;
+            }
+            break;
+            case TOKEN_KIND_OPEN_QUOTATION:
+            {
+                if (input[pos - 1] != '\\')
+                    current_token_kind = TOKEN_KIND_CLOSE_QUOTATION;
+                else
+                    goto process_char;
+            }
+            break;
+            case TOKEN_KIND_COLON:
+            {
+                current_token_kind = TOKEN_KIND_OPEN_QUOTATION;
+                state = STATE_VALUE;
+            }
+            break;
+            default:
+            {
+                JP_PANIC_RAW();
+            }
+            break;
+            }
+        }
+        break;
+        case ',':
+        {
+            if (current_token_kind == TOKEN_KIND_CLOSE_QUOTATION ||
+                current_token_kind == TOKEN_KIND_CLOSE_CURLY)
+            {
+                jobject_add_pair(&json, current_key, current_value);
+                current_token_kind = TOKEN_KIND_COMMA;
+            }
+            else
+            {
+                goto process_char;
+            }
+        }
+        break;
+        case ':':
+        {
+            if (current_token_kind == TOKEN_KIND_CLOSE_QUOTATION)
+                current_token_kind = TOKEN_KIND_COLON;
+            else
+                goto process_char;
+        }
+        break;
+        case '{':
+        {
+            switch (current_token_kind)
+            {
+            case TOKEN_KIND_NONE:
+            {
+                size_t i = pos;
+                do
+                {
+                    i++;
+                    if (input[i] == '\0')
+                        JP_PANIC("unexpected end of file at %zu", i);
+                    if (input[i] == '{')
+                    {
+                        size_t open_curly_count = 2;
+                        do
+                        {
+                            i++;
+                            if (input[i] == '\0')
+                                JP_PANIC("unexpected end of file at %zu", i);
+                            else if (input[i] == '{')
+                                open_curly_count++;
+                            else if (input[i] == '}')
+                                open_curly_count--;
+                        } while (open_curly_count != 1);
+                        i++;
+                    }
+                    else if (input[i] == ':' && input[i - 1] == '"' && input[i - 2] != '\\')
+                        jparser->pairs_commited++;
+                } while (input[i] != '}');
+                current_token_kind = TOKEN_KIND_OPEN_CURLY;
+            }
+            break;
+            case TOKEN_KIND_COLON:
+            {
+                size_t start_pos = pos;
+                size_t open_curly_count = 1;
+                do
+                {
+                    pos++;
+                    if (input[pos] == '\0')
+                        JP_PANIC("unexpected end of file at %zu", pos);
+                    else if (input[pos] == '{')
+                        open_curly_count++;
+                    else if (input[pos] == '}')
+                        open_curly_count--;
+                } while (open_curly_count != 0);
+                size_t nested_object_string_size = pos - start_pos + 2;
+                char *nested_object_string = (char *)memory_alloc(&jparser->json_memory, sizeof(nested_object_string_size));
+                memcpy(nested_object_string, input + start_pos, nested_object_string_size - 1);
+                nested_object_string[nested_object_string_size - 1] = '\0';
+                JObject nested_object = parse_object(jparser, nested_object_string);
+                current_value = (JValue *)memory_alloc(&jparser->json_memory, sizeof(JValue));
+                current_value->type = JSON_OBJECT;
+                current_value->object = nested_object;
+                current_token_kind = TOKEN_KIND_CLOSE_CURLY;
+                state = STATE_NONE;
+            }
+            break;
+            default:
+                goto process_char;
+            }
+        }
+        break;
+        case '}':
+        {
+            if (current_token_kind == TOKEN_KIND_CLOSE_QUOTATION ||
+                current_token_kind == TOKEN_KIND_CLOSE_CURLY)
+            {
+                jobject_add_pair(&json, current_key, current_value);
+                current_token_kind = TOKEN_KIND_CLOSE_CURLY;
+            }
+            else
+            {
+                goto process_char;
+            }
+        }
+        break;
+        default:
+        {
+            switch (current_token_kind)
+            {
+            case TOKEN_KIND_OPEN_QUOTATION:
+            {
+            process_char:
+                if (state == STATE_KEY)
+                {
+                    size_t start_pos = pos;
+                    do
+                    {
+                        pos++;
+                        if (input[pos] == '\0')
+                            JP_PANIC("unexpected end of file at %zu", pos);
+                    } while (input[pos] != '"' && input[pos - 1] != '\\');
+                    size_t key_size = pos - start_pos + 1;
+                    current_key = (char *)memory_alloc(&jparser->json_memory, key_size);
+                    memcpy(current_key, input + start_pos, key_size - 1);
+                    current_key[key_size - 1] = '\0';
+                    current_token_kind = TOKEN_KIND_CLOSE_QUOTATION;
+                    state = STATE_NONE;
+                }
+                else if (state == STATE_VALUE)
+                {
+                    size_t start_pos = pos;
+                    do
+                    {
+                        pos++;
+                        if (input[pos] == '\0')
+                            JP_PANIC("unexpected end of file at %zu", pos);
+                    } while (input[pos] != '"' && input[pos - 1] != '\\');
+                    size_t value_size = pos - start_pos + 1;
+                    char *value_string = (char *)memory_alloc(&jparser->json_memory, value_size);
+                    memcpy(value_string, input + start_pos, value_size - 1);
+                    value_string[value_size - 1] = '\0';
+                    current_value = (JValue *)memory_alloc(&jparser->json_memory, sizeof(JValue));
+                    current_value->type = JSON_STRING;
+                    current_value->string = value_string;
+                    current_token_kind = TOKEN_KIND_CLOSE_QUOTATION;
+                    state = STATE_NONE;
+                }
+            }
+            break;
+            default:
+            {
+                JP_PANIC_RAW();
+            }
+            break;
+            }
+        }
+        break;
+        }
+    }
+    return json;
+}
+
+#endif // JP_IMPLEMENTATION
---
+++ test.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define JP_IMPLEMENTATION
+#include "jp.h"
+
+char *read_file_as_str(const char *path)
+{
+    FILE *f = fopen(path, "rb");
+    fseek(f, 0, SEEK_END);
+    size_t size = ftell(f);
+    char *str = (char *)malloc(size + 1);
+    memset(str, 0, size);
+    fseek(f, 0, SEEK_SET);
+    fread(str, 1, size, f);
+    str[size] = '\0';
+    fclose(f);
+    return str;
+}
+
+int main()
+{
+    char *input = read_file_as_str("input.json");
+    printf("%s\n", input);
+
+    JParser parser;
+    jparser_init(&parser, input);
+
+    JObject json = parse_object(&parser, input);
+
+    JValue value = jobject_get(&json, "test");
+
+    printf("value: %d\n", value.boolean);
+
+    // JsonParser parser(input);
+    // JObject json = parser.parse();
+
+    // printf("%d\n", (bool &&)json["test"]);
+    // printf("%d\n", json.boolean("test"));
+
+    // char *string = (char *&&)json["owo"]["deep"]["dark"]["dungeon"];
+    // printf("%s\n", string);
+
+    // printf("%s\n", json.obj("owo").str("uwu"));
+
+    // parser.free();
+    // free(input);
+
+    return 0;
+}
