f1aa26b3a1a417ea440f165340c0edeca9a48331
Ciremun <Ciremun@yandex.ru>
2021-08-24 00:04:44 +0300

    fmt

--- test.cpp
+++ test.cpp
@@ -45,14 +45,14 @@ int main()
 
     // C
     JValue value = json_get(&json.object, "error");
-    if (value.type == JSON_ERROR)
-        printf("error: %s\n", json_get_error(&value.error));
     JObject owo = json_get(&json.object, "owo").object;
     JObject uwo = json_get(&owo, "uwo").object;
     char *str = json_get(&uwo, "str").string;
     JValue *arr = json_get(&uwo, "arr").array;
     JValue *test = json_get(&json.object, "test").array;
     printf("  C:\n");
+    if (value.type == JSON_ERROR)
+        printf("    error: %s\n", json_get_error(&value.error));
     printf("    string: %s\n", str);
     printf("    array string: %s\n", arr[0].string);
     printf("    negative number: %lld\n", test[0].number);
