b262ca812befccfb556661f4ce0be0cfdc16f233
Ciremun <Ciremun@yandex.ru>
2021-08-31 23:10:07 +0300

    #14 nested value access

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,3 +1,5 @@
+#define NDEBUG
+
 #define JMEM_IMPLEMENTATION
 #include "../jmem.h"
 
@@ -8,7 +10,34 @@
 
 size_t total_errors;
 
+void test_errors()
+{
+    const char* input = "{}";
+
+    JMemory *memory = jmem_init();
+    TEST(memory != 0);
+
+    JParser parser = json_init(memory, input);
+    JValue json = json_parse(&parser);
+
+    if (TEST(json.type == JSON_OBJECT))
+    {
+        JValue nested_error = json["deep"][6]["dark"][9]["error"];
+        if (TEST(nested_error.type == JSON_ERROR))
+            TEST(nested_error.error == JSON_TYPE_ERROR);
+    }
+
+    jmem_free(memory);
+}
+
+Test tests[] = {
+    { "errors", test_errors },
+};
+
 int main()
 {
-    return 0;
+    for (size_t i = 0; i < COUNT(tests); ++i)
+        tests[i].f();
+
+    TOTAL_ERRORS
 }
