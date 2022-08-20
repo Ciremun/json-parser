d34976a80de32daeca2d1effb195d4ff8312a8d4
Ciremun <Ciremun@yandex.ru>
2022-08-20 11:49:45 +0300

    fix cpp tests

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,8 +1,3 @@
-#define NDEBUG
-
-#define JMEM_IMPLEMENTATION
-#include "../jmem.h"
-
 #define JP_IMPLEMENTATION
 #include "../jp.h"
 
@@ -14,10 +9,10 @@ void test_errors()
 {
     const char *input = "{}";
 
-    JMemory *memory = jmem_init();
-    TEST(memory != 0);
+    JMemory memory;
+    memory.alloc = malloc;
 
-    JParser parser = json_init(memory, input);
+    JParser parser = json_init(&memory, input);
     JValue json = json_parse(&parser);
 
     if (TEST(json.type == JSON_OBJECT))
@@ -27,18 +22,16 @@ void test_errors()
         if (TEST(nested_error.type == JSON_ERROR))
             TEST(nested_error.error == JSON_TYPE_ERROR);
     }
-
-    jmem_free(memory);
 }
 
 void test_values()
 {
     const char *input = "{\"test\": 69}";
 
-    JMemory *memory = jmem_init();
-    TEST(memory != 0);
+    JMemory memory;
+    memory.alloc = malloc;
 
-    JParser parser = json_init(memory, input);
+    JParser parser = json_init(&memory, input);
     JValue json = json_parse(&parser);
 
     if (TEST(json.type == JSON_OBJECT))
@@ -47,88 +40,60 @@ void test_values()
         if (TEST(object_number.type == JSON_NUMBER))
             TEST(object_number.number == 69);
     }
-
-    jmem_free(memory);
 }
 
 void test_single_values()
 {
     const char *input = "1337";
 
-    JMemory *memory = jmem_init();
+    JMemory memory;
+    memory.alloc = malloc;
 
-    JParser parser = json_init(memory, input);
+    JParser parser = json_init(&memory, input);
     JValue value = json_parse(&parser);
 
     if (TEST(value.type == JSON_NUMBER))
         TEST(value.number == 1337);
 
-    jmem_free(memory);
-
-    memory = jmem_init();
-    TEST(memory != 0);
-
     input = "\"string\"";
-    parser = json_init(memory, input);
+    parser = json_init(&memory, input);
 
     value = json_parse(&parser);
 
     if (TEST(value.type == JSON_STRING))
         TEST(strcmp(value.string, "string") == 0);
 
-    jmem_free(memory);
-
-    memory = jmem_init();
-    TEST(memory != 0);
-
     input = "[]";
-    parser = json_init(memory, input);
+    parser = json_init(&memory, input);
 
     value = json_parse(&parser);
 
     if (TEST(value.type == JSON_ARRAY))
         TEST(value.array == 0);
 
-    jmem_free(memory);
-
-    memory = jmem_init();
-    TEST(memory != 0);
-
     input = "{}";
-    parser = json_init(memory, input);
+    parser = json_init(&memory, input);
 
     value = json_parse(&parser);
 
     if (TEST(value.type == JSON_OBJECT))
         TEST(value.length == 0);
 
-    jmem_free(memory);
-
-    memory = jmem_init();
-    TEST(memory != 0);
-
     input = "true";
-    parser = json_init(memory, input);
+    parser = json_init(&memory, input);
 
     value = json_parse(&parser);
 
     if (TEST(value.type == JSON_BOOL))
         TEST(value.boolean == 1);
 
-    jmem_free(memory);
-
-    memory = jmem_init();
-    TEST(memory != 0);
-
     input = "null";
-    parser = json_init(memory, input);
+    parser = json_init(&memory, input);
 
     value = json_parse(&parser);
 
     if (TEST(value.type == JSON_NULL))
         TEST(value.null == 0);
-
-    jmem_free(memory);
 }
 
 Test tests[] = {
